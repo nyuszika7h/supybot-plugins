b295965132f6fcfaf9d02d35e051d56aaf0d0dd6
nyuszika7h <nyuszika7h@cadoth.net>
2012-12-02 15:40:34 +0100

    MCStatus: Second attempt to fix the previous issue
    This is a hacky solution, as the separator is now always surrounded by
    spaces, and the service suffix has a space on the right. Unfortunately
    this is the best I can do for now.

--- MCStatus/config.py
+++ MCStatus/config.py
@@ -27,31 +27,31 @@ MCStatus = conf.registerPlugin('MCStatus')
 # conf.registerGlobalValue(MCStatus, 'someConfigVariableName',
 #     registry.Boolean(False, _("""Help for someConfigVariableName.""")))
 conf.registerGlobalValue(MCStatus, 'statusURL',
-        registry.StringSurroundedBySpaces('http://status.mojang.com/check', _("""This is the URL
+        registry.String('http://status.mojang.com/check', _("""This is the URL
         that is queried for JSON data. You shouldn't change this unless you
         know what you are doing.""")))
 conf.registerChannelValue(MCStatus, 'prefix',
-        registry.StringSurroundedBySpaces('', _("""This text is prepended to the mcstatus
+        registry.String('', _("""This text is prepended to the mcstatus
         command's output.""")))
 conf.registerChannelValue(MCStatus, 'suffix',
-        registry.StringSurroundedBySpaces('', _("""This text is appended to the mcstatus
+        registry.String('', _("""This text is appended to the mcstatus
         command's output.""")))
 conf.registerChannelValue(MCStatus, 'separator',
-        registry.StringSurroundedBySpaces(' | ', _("""This text is inserted between service-status
+        registry.StringSurroundedBySpaces('|', _("""This text is inserted between service-status
         pairs.""")))
 conf.registerGroup(MCStatus, 'service')
 conf.registerChannelValue(MCStatus.service, 'prefix',
-        registry.StringSurroundedBySpaces('', _("""This text is prepended to each service's
+        registry.String('', _("""This text is prepended to each service's
         name.""")))
 conf.registerChannelValue(MCStatus.service, 'suffix',
-        registry.StringSurroundedBySpaces(': ', _("""This text is appended to each service's
+        registry.StringWithSpaceOnRight(':', _("""This text is appended to each service's
         name.""")))
 conf.registerGroup(MCStatus, 'status')
 conf.registerChannelValue(MCStatus.status, 'online',
-        registry.StringSurroundedBySpaces('\x0309ONLINE\x03', _("""This text is displayed when a
+        registry.NormalizedString('\x0309ONLINE\x03', _("""This text is displayed when a
         service is online (green).""")))
 conf.registerChannelValue(MCStatus.status, 'offline',
-        registry.StringSurroundedBySpaces('\x0304OFFLINE\x03', _("""This text is displayed when a
+        registry.NormalizedString('\x0304OFFLINE\x03', _("""This text is displayed when a
         service is offline (red).""")))
 
 # vim: set shiftwidth=4 tabstop=4 expandtab textwidth=79:
